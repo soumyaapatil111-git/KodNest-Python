c67398dec17d2a8e5c0c1fa63799b79689c4c6a2
Soumya Patil <soumyaapatil111@gmail.com>
2026-09-09 15:51:44 +0530

    d6_largest

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_prg.py
@@ -0,0 +1,14 @@
+print("This is my first program")
+
+print("my name is soumya")
+print("my age is 22")
+print("bangalore is my city")
+print("python is my favorite programming language")
+print("it was the best experience, i have learnt psudocode, learnt basics topics of python.")
+
+print("*")
+print("* *")
+print("* * *") 
+print("* * * *")
+print("* * * * *")
+
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/pseudo.py
@@ -0,0 +1,28 @@
+# pint hello world
+# print whether number is even or odd
+# to find number is positive , negative or zero
+# to find larhest among a number
+
+
+'''start
+input a
+input b
+input c
+if a >=b and a>=c;
+print "a" is largest number
+elif b>=a and b>=c;
+print "b" is largest number
+else c>=a and c>=b;
+print "c" is largest number
+end'''
+
+a=10; b=20; c=30
+if a >= b and a>=c:
+    print ("a is largest")
+elif b>=a and b>=c:
+    print ("b is largest number")
+else :
+    print ("c is largest number")
+
+
+
