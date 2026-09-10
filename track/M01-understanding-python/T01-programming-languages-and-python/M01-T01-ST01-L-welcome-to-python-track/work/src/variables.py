874ade3a2e0a43baf1c0b5ea8ee3bdecd7b43496
Soumya Patil <soumyaapatil111@gmail.com>
2026-09-10 15:18:40 +0530

    var

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/variables.py
@@ -0,0 +1,22 @@
+name = "soumya"
+name = "sam"
+print(name)
+
+age = 21
+# my name is sam and age is 21:
+print("my name is " + " and age is" , age ,"!")
+print("my name is " + " and age is" , str(age) + "!")
+
+print(f"my name is {name} and age is {age}!")
+marks = 50
+marks = marks + 50
+print(marks)
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+
+x = y = z = 100
+print(x, y, z)
+
+print(x + a)
+print(x + y)
